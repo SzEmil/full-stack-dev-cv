c1b1f7722266879b19ebf5801e4ab39d18dddd7a
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2024-03-20 12:00:17 +0100

    Update getPreviewURL.sh

--- CI-CD/scripts/getPreviewURL.sh
+++ CI-CD/scripts/getPreviewURL.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 
-# Pobierz adres URL podglądu z logów
-preview_url=$(grep -Eo "Preview: https:\/\/(.*)" <<< "$BUILD_LOG")
+# Pobierz cały log kompilacji do zmiennej
+build_log="$BUILD_LOG"
+echo $build_log
+# Użyj sed zamiast grep, aby wyodrębnić adres URL podglądu, uwzględniając znaki specjalne
+preview_url=$(sed -n 's/^.*Preview: https:\/\/\([^"]*\).*/\1/p' <<< "$build_log")
 
-# Usuń znaki specjalne z adresu URL podglądu
-preview_url=${preview_url##*:}
+# Sprawdź, czy istnieje adres URL podglądu
+if [ -z "$preview_url" ]; then
+  echo "Nie znaleziono adresu URL podglądu w logach kompilacji."
+  exit 1
+fi
 
+# Wydrukuj adres URL podglądu
 echo "previewURL to: $preview_url"
-
