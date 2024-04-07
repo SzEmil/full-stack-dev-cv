5a4a17f4fbd6495ada7c0e178210e861514004aa
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2024-04-07 16:39:08 +0200

    feat(blur url): Images Blur Url Data

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,9 +1,10 @@
-
 import { Routes } from '@/constants/endpoints';
 import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 import { RiPagesLine } from 'react-icons/ri';
 import { HoverCard } from './components/HoverCard';
+import Image from 'next/image';
+import { dynamicBlurDataUrl } from '@/helpers/dynamicBlurDataUrl';
 
 type ProjectCardProps = {
   id: string;
@@ -15,7 +16,7 @@ type ProjectCardProps = {
   git: string;
 };
 
-export const ProjectCard = ({
+export const ProjectCard = async ({
   id,
   name,
   picture,
@@ -24,18 +25,30 @@ export const ProjectCard = ({
   web,
   git,
 }: ProjectCardProps) => {
+  const blurURL = await dynamicBlurDataUrl(picture)
   return (
     <div className="lg:w-[450px] lg:h-[280px] w-[350px] h-[200px] rounded-md hover:border relative group ">
       <Link href={`${Routes.projects}/${id}`}>
         <div
-          style={{ backgroundImage: `url(${picture})` }}
-          className="w-full  h-full  bg-cover bg-center text-white rounded-lg"
+        //  style={{ backgroundImage: `url(${picture})` }}
+          className="w-full  h-full  bg-cover bg-center text-white rounded-lg relative"
         >
-          <div className=" inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-80 transition-opacity">
-            <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center flex-col">
-              <HoverCard name={name} web={web} git={git} />
-            </div>
+        <Image
+          src={picture}
+          alt={name}
+          placeholder='blur'
+          blurDataURL={blurURL}
+          objectFit='cover'
+          layout='fill'
+          objectPosition='center'
+          className="w-full  h-full  text-white rounded-lg absolute left-0 top-0"
+        />
+        <div className=" inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-80 transition-opacity">
+          <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center flex-col">
+            <HoverCard name={name} web={web} git={git} />
           </div>
+        </div>
+
         </div>
       </Link>
     </div>
